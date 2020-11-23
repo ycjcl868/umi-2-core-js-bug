30c5f42014d54883fbd681072d0c54847d2a4ebe
ycjcl868 <45808948@qq.com>
2020-11-23 20:29:21 +0800

    chore:

--- config/config.ts
+++ config/config.ts
@@ -1,14 +1,7 @@
-import { IConfig } from 'umi-types';
-
 export default {
-  hash: true,
-  targets: {
-    ie: 11,
-  },
-  // umi routes: https://umijs.org/zh/guide/router.html
   routes: [
     {
       component: './404',
     },
   ],
-} as IConfig;
+};
